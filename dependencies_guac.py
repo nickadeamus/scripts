4e5c9dfbe4b852f09903de7469541b4af05c8370
nickadeamus <129408586+nickadeamus@users.noreply.github.com>
2024-10-18 09:13:16 -0600

    Update dependencies_guac.py

--- dependencies_guac.py
+++ dependencies_guac.py
@@ -1,22 +1,26 @@
 import subprocess
-import sys
 
 # List of packages you want to install
 packages = [
-    'cairo-devel', #Cairo
-    'libjpeg-turbo-devel', #LibTurboJPEG
-    'libpng-devel', #LibPNG
-    'libtool' #LibTool
-    'libuuid-devel', #LibUUID
-    'freerdp-devel', #FreeRDP
-    'pango-devel', #Pango
-    'libssh2-devel', #LibSSH
-    'openssl-devel', #OpenSSL
+    'cairo-devel',
+    'libjpeg-turbo-devel',
+    'libpng-devel',
+    'libtool',
+    'libuuid-devel',
+    'freerdp-devel',
+    'pango-devel',
+    'libssh2-devel',
+    'openssl-devel'
 ]
 
 def install_packages(package_list):
-    for package in package_list:
-        subprocess.check_call([sys.executable, '-m', 'dnf', 'install', package])
+    command = ['sudo', 'dnf', 'install', '-y'] + package_list
+    try:
+        subprocess.check_call(command)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        print("Command output:")
+        print(e.output)
 
 if __name__ == "__main__":
     install_packages(packages)
