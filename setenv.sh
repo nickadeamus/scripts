7ee1fc4eace1a29d968219049410e08c1f8ba21a
nickadeamus <129408586+nickadeamus@users.noreply.github.com>
2024-11-18 16:28:19 -0700

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,5 @@
-must be in root/bin
-
-CATALINA_HOME = /bin
-JAVA_HOME = /usr/java
+#!/bin/sh
+export JAVA_HOME=/usr/java
+export CATALINA_HOME=/var/lib/tomcat/bin
+export CATALINA_BASE=/var/lib/tomcat/bin
+export JAVA_OPTS="-Xms512m -Xmx1024m"
